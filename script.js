e505ed47a491505f22fb49ae699572eb27c782cb
LorenaFHGR <lorena.staiger@gmail.com>
2022-01-18 14:27:59 +0100

    final touches

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 /* Dies ist das Script für unser Mühle-Spiel. 
-Code abgeleitet von externen Ressourcen ist entsprechend gekennzeichnet. 
+Die Theorie des Codes stammt von W3Schools, JavaScript Tutorial. 
+Das unbenutzte Mapping ist von der Schach-Anleitung abgeleitet (siehe Quellenverzeichnis der Dokumentation) 
 Die Funktionen sind einzeln beschrieben.*/
 
 
 // ----------------------------- Funktion 1 Anfang ----------------------------- //
 /* 
-Drag and drop der Spielfiguren
+Drag and drop der Spielfiguren.
 */
 
-function allowDrop(event) {
+function allowDrop(event) { /* Dadurch wird im HTML gekennzeichnet, wo gedroppt werden darf (nicht auf alle Zellen erlaubt) */
     event.preventDefault();
 }
 
 function drag(event) {
-    event.dataTransfer.setData("text", event.target.id);
+    event.dataTransfer.setData("text", event.target.id); /* Die Metadaten des gedraggten Objektes werden kopiert */
 }
 
 function drop(event) {
     event.preventDefault();
-    var data = event.dataTransfer.getData("text");
-    event.target.appendChild(document.getElementById(data));
+    var data = event.dataTransfer.getData("text"); /* Metadaten werden geholt... */
+    event.target.appendChild(document.getElementById(data)); /* ...und eingefügt */
 }
 
 // ----------------------------- Funktion 1 Ende ----------------------------- //
 
 // ----------------------------- Funktion 2 Anfang ----------------------------- //
 /* 
-Event-Listener hinzufügen für den Refresh-Button, um die Seite (und somit das Spiel) auf Knopfdruck
-neu zu laden bzw. neu zu beginnen
+Event-Listener hinzugefügt für den Refresh-Button, um die Seite (und somit das Spiel) auf Knopfdruck
+neu zu laden bzw. neu zu beginnen. Das refresh-div wird anhand des querySelectors ausgewählt. 
 */
 
 document.querySelector(".refresh").addEventListener("click", function reload (){
@@ -48,7 +49,7 @@ document.querySelector(".refresh").style.cursor="pointer";
 Mapping der Daten Bretts in der Tabelle, analog zu den ID's im HTML-Code
 
 Anmerkung: Dieses Mapping wurde nicht verwendet. Es sollte für das Hintergrund-Mapping des Spiels
-dienlich sein, um Spielzüge zu erkennen und speichern. Es wurde absichtlich drin gelassen, um den Versuch
+dienlich sein, um Spielzüge zu erkennen und (evtl.) zu speichern. Es wurde absichtlich drin gelassen, um den Versuch
 zu veranschaulichen und für allfällige Erweiterungen im nächsten WEBP-Kurs. 
 */
 
